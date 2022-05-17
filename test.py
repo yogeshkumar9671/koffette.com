7edb79d134ece61035a30f12be4393d732901cc5
yogeshkumar9671 <97506050+yogeshkumar9671@users.noreply.github.com>
2022-05-17 13:00:01 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+
+
+    <div class="swiper mySwiper swiper-initialized swiper-horizontal swiper-pointer-events swiper-backface-hidden">
+        <div class="swiper-wrapper" id="swiper-wrapper-9cf175cca5e108eb5" aria-live="off" style="transform: translate3d(-7375px, 0px, 0px); transition-duration: 300ms;">
+            <div class="swiper-slide swiper-slide-duplicate swiper-slide-next swiper-slide-duplicate-prev" data-swiper-slide-index="3" role="group" aria-label="4 / 4" style="width: 1475px;"><img src="/static/app/images/banner/banner3.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="swiper-slide swiper-slide-duplicate-active" data-swiper-slide-index="0" role="group" aria-label="1 / 4" style="width: 1475px;"><img src="/static/app/images/banner/banner4.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="swiper-slide" data-swiper-slide-index="1" role="group" aria-label="2 / 4" style="width: 1475px;"><img src="/static/app/images/banner/banner1.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="swiper-slide" data-swiper-slide-index="2" role="group" aria-label="3 / 4" style="width: 1475px;"><img src="/static/app/images/banner/banner2.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="swiper-slide swiper-slide-prev swiper-slide-duplicate-next" data-swiper-slide-index="3" role="group" aria-label="4 / 4" style="width: 1475px;"><img src="/static/app/images/banner/banner3.jpg" class="d-block w-100" alt="...">
+            </div>
+            <!-- <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div> -->
+            <div class="swiper-slide swiper-slide-duplicate swiper-slide-active" data-swiper-slide-index="0" role="group" aria-label="1 / 4" style="width: 1475px;"><img src="/static/app/images/banner/banner4.jpg" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span></div>
+
+
+</body>
+
+</html>
